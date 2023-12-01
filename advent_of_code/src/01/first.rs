364cd940add8e79b889ee7300f1c98b0431383ba
Julian Schumacher <professional@julianschumacher.dev>
2023-12-01 21:16:22 +0100

    01 - first problem solved

---
+++ advent_of_code/src/01/first.rs
@@ -0,0 +1,27 @@
+use std::fs;
+
+fn first() {
+    let file_content  = fs::read_to_string("src/01/content.txt").expect("Error");
+    let split = file_content.split("\n");
+    let mut total : u64 = 0;
+    for line in split {
+        let mut one : char = 'A';
+        let mut two : char = 'B';
+        let mut is_first: bool = true;
+        for char in line.chars() {
+            if char.is_numeric() {
+                if is_first {
+                    is_first = false;
+                    one = char;
+                }
+                two = char;
+            }
+        }
+        let mut string = one.to_string();
+        string.push(two);
+        println!("{}", string);
+        let int : u64 = string.parse::<u64>().unwrap();
+        total += int;
+    }
+    println!("{}", total);
+}
