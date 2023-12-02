4e71fa909059c6f5d5b3223c495d3a3d7bfe5bb3
Julian Schumacher <professional@julianschumacher.dev>
2023-12-02 09:16:05 +0100

    Update first_december.rb

--- ruby/advent_of_code/days/01/first_december.rb
+++ ruby/advent_of_code/days/01/first_december.rb
@@ -5,15 +5,28 @@ class FirstDecember
 
   # Call this to solve the first problem of this day
   def self.first
+    read_file.each do |line|
+      first_number = 0
+      last_number = 0
+      total = 0
+      is_first_number = true
+      line.each_char do |char|
+        if char.match?(/[[:digit:]]/)
+          if is_first_number
+            is_first_number = false
+            first_number = char
+          end
+          last_number = char
+        end
+        number = first_number.to_s + last_number.to_s
+        total += number.to_i
+      end
+    end
   end
 
   # Read the File and return it's content
-  def read_file
-    file = File.open('content.txt')
-    file_content = file.read
-    file_content.lines.each { |line|
-
-    }
+  def self.read_file
+    File.open('content.txt').readlines
   end
 
   # Solves the second problem of the first day
